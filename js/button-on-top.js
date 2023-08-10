c50f4ed23665aebadad7b457c6292f14a23cbfab
Evgen Zaharevich <metrovyy@gmail.com>
2023-08-10 12:25:18 +0300

    added hide button on-top

--- js/button-on-top.js
+++ js/button-on-top.js
@@ -1,10 +1,20 @@
 const BtnOnTop = document.querySelector(".on-top");
 
+const clientViewPortHeightValue = document.documentElement.clientHeight;
+
 const onTop = () => {
   if (window.scrollY > 0) {
     window.scrollBy(0, -50);
-    onTop();
+    setTimeout(onTop, 0);
   }
 };
-
 BtnOnTop.addEventListener("click", onTop);
+
+const showButtonOnTop = () => {
+  if (window.scrollY > clientViewPortHeightValue) {
+    BtnOnTop.classList.add("on-top--show-btn");
+  } else {
+    BtnOnTop.classList.remove("on-top--show-btn");
+  }
+};
+window.addEventListener("scroll", showButtonOnTop);
