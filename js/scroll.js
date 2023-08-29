f589fbbffba0f2990f11700d791f4bf82bc91520
Evgen Zaharevich <metrovyy@gmail.com>
2023-08-29 15:12:48 +0300

    fix style for adaptive

--- js/scroll.js
+++ js/scroll.js
@@ -11,69 +11,71 @@ ScrollSmoother.create({
   effects: true,
 });
 
-gsap.fromTo(
-  ".hero-section",
-  { opacity: 1 },
-  {
-    opacity: 0,
-    scrollTrigger: {
-      trigger: ".hero-section",
-      start: "center",
-      end: "820",
-      scrub: true,
-    },
-  }
-);
-
-let itemsLeftColumn = gsap.utils.toArray("gallery__left .gallery__item");
-
-itemsLeftColumn.forEach((item) => {
+if (ScrollTrigger !== 1) {
   gsap.fromTo(
-    item,
+    ".hero-section",
+    { opacity: 1 },
+    {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".hero-section",
+        start: "center",
+        end: "820",
+        scrub: true,
+      },
+    }
+  );
+
+  let itemsLeftColumn = gsap.utils.toArray("gallery__left .gallery__item");
+
+  itemsLeftColumn.forEach((item) => {
+    gsap.fromTo(
+      item,
+      { x: -100, opacity: 0 },
+      {
+        x: 0,
+        opacity: 1,
+        scrollTrigger: {
+          trigger: item,
+          start: "-3000",
+          end: "-500",
+          scrub: true,
+        },
+      }
+    );
+  });
+
+  let itemsRightColumn = gsap.utils.toArray(".gallery__right .gallery__item");
+
+  itemsRightColumn.forEach((item) => {
+    gsap.fromTo(
+      item,
+      { x: 100, opacity: 0 },
+      {
+        x: 0,
+        opacity: 1,
+        scrollTrigger: {
+          trigger: item,
+          start: "-3000",
+          end: "-100",
+          scrub: true,
+        },
+      }
+    );
+  });
+
+  gsap.fromTo(
+    ".image-brushes",
     { x: -100, opacity: 0 },
     {
       x: 0,
       opacity: 1,
       scrollTrigger: {
-        trigger: item,
-        start: "-3000",
-        end: "-500",
+        trigger: ".image-brushes",
+        start: "-850",
+        end: "-250",
         scrub: true,
       },
     }
   );
-});
-
-let itemsRightColumn = gsap.utils.toArray(".gallery__right .gallery__item");
-
-itemsRightColumn.forEach((item) => {
-  gsap.fromTo(
-    item,
-    { x: 100, opacity: 0 },
-    {
-      x: 0,
-      opacity: 1,
-      scrollTrigger: {
-        trigger: item,
-        start: "-3000",
-        end: "-100",
-        scrub: true,
-      },
-    }
-  );
-});
-
-gsap.fromTo(
-  ".image-brushes",
-  { x: -100, opacity: 0 },
-  {
-    x: 0,
-    opacity: 1,
-    scrollTrigger: {
-      trigger: ".image-brushes",
-      start: "-850",
-      end: "-250",
-      scrub: true,
-    },
-  }
-);
+}
